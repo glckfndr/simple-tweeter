423254fe6bda0134e128c8dd3d70d26e67298a30
glckfndr <obulan65@gmail.com>
2024-12-11 21:20:32 +0200

    feat: add name and password validation for User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || !password.nil? }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,23 +4,23 @@ RSpec.describe User, type: :model do
   describe 'validation' do
     context 'positive tests' do
       it 'is authenticated with valid credentionlas' do
-        user = build(user:)
-        expect(user ).to be_valid
+        user = build(:user)
+        expect(user).to be_valid
       end
     end
     context 'negative tests' do
       it 'is invalid without name' do
-        user = build(user:, name: nil)
+        user = build(:user, name: nil)
         expect(user).not_to be_valid
       end
 
       it 'is invalid without email' do
-        user = build(user:, email: nil)
+        user = build(:user, email: nil)
         expect(user).not_to be_valid
       end
 
       it 'is invalid without password' do
-        user = build(user:, password: nil)
+        user = build(:user, password: nil)
         expect(user).not_to be_valid
       end
 
