af8b1e1f585263d91b0f3eaba1f45e1704453700
glckfndr <obulan65@gmail.com>
2024-12-12 23:23:26 +0200

    feat: update User model to ensure all tests pass
    Add uniqueness to username and email, add method validate_email and validate_password with regexp.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
-    username { Faker::Lorem.characters(number: 3, min_alpha: 3) }
+    username { Faker::Internet.username(specifier: 3)  }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 8, mix_case: true, special_characters: true) }
+    password { Faker::Internet.password(min_length: 8, mix_case: true,
+    special_characters: true) }
   end
 end
