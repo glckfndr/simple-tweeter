57c3f2f5a779f4bdab9774e3bd8e1b6d6b7de83b
glckfndr <obulan65@gmail.com>
2024-12-12 22:28:44 +0200

    test: update tests for email standards and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,50 +7,85 @@ RSpec.describe User, type: :model do
         user = build(:user)
         expect(user).to be_valid
       end
+
+      it 'is valid when a password contains any characters repeat up to twice' do
+        user = build(:user, password: "22334455##")
+        expect(user).to be_valid
+      end
+
+      it 'is valid when a password contains at least one symbol' do
+        user = build(:user, password: "@#≈™]€*([@")
+        expect(user).to be_valid
+      end
     end
+
     context 'negative tests' do
-      it 'is invalid without name' do
-        user = build(:user, name: nil)
+      describe 'username' do
+        it 'is invalid when nil' do
+          user = build(:user, username: nil)
+          expect(user).not_to be_valid
+        end
+
+        it 'is invalid when empty' do
+          user = build(:user, username: '')
+          expect(user).not_to be_valid
+        end
+
+        it 'is invalid when it is short (<3)' do
+          user = build(:user, username: 'ra')
+          expect(user).not_to be_valid
+        end
+      end
+
+      it 'is invalid when the user with the same username is already in db' do
+        create(:user, username: "gal")
+        user = build(:user, username: "gal")
         expect(user).not_to be_valid
       end
 
-      it 'is invalid without email' do
-        user = build(:user, email: nil)
-        expect(user).not_to be_valid
+      describe 'email' do
+        it 'is when email is nil' do
+          user = build(:user, email: nil)
+          expect(user).not_to be_valid
+        end
+
+        it 'is invalid when the same email exists' do
+          create(:user, email: 'user@test.com')
+          user = build(:user, email: 'user@test.com')
+          expect(user).not_to be_valid
+        end
+
+        it 'is invalid when it is improperly formatted' do
+          improper_email = ['abcdef', 'abcdef@', 'abcdef@.', 'abcdef@com','abcdef@.com','abcdef@com.', '@abcdef.com']
+          improper_email.each do |email|
+            user = build(:user, email: email)
+            expect(user).not_to be_valid, "#{email.inspect} should be invalid"
+          end
+        end
       end
 
-      it 'is invalid without password' do
-        user = build(:user, password: nil)
-        expect(user).not_to be_valid
+      describe 'password' do
+        it 'is invalid without password' do
+          user = build(:user, password: nil)
+          expect(user).not_to be_valid
+        end
+
+        it 'is invalid when it contains less then 8 characters' do
+          user = build(:user, password: '@#≈™]€*')
+          expect(user).not_to be_valid
+        end
+
+        it 'is invalid when contains any characters repeat more than twice' do
+          user = build(:user, password: "22234455##")
+          expect(user).not_to be_valid
+        end
+
+        it 'is invalid when does not contain at least one symbol' do
+          user = build(:user, password: "jl4ksDfojwkx2t5")
+          expect(user).not_to be_valid
+        end
+
       end
-
-      it 'is invalid with an empty name' do
-        user = build(:user, name: '')
-        expect(user).not_to be_valid
-      end
-
-
-      it 'is invalid with the same email' do
-        create(:user, email: 'user@test.com')
-        user = build(:user, email: 'user@test.com')
-        expect(user).not_to be_valid
-      end
-
-      it 'is invalid with a short password (<8)' do
-        user = build(:user, password: 'Aa#1234')
-        expect(user).not_to be_valid
-      end
-
-      it 'is invalid with a short name (<3)' do
-        user = build(:user, name: 'ra')
-        expect(user).not_to be_valid
-      end
-
-      it 'is invalid with an improperly formatted email' do
-        user = build(:user, email: 'invalid_email')
-        expect(user).not_to be_valid
-      end
-
     end
 
   end
