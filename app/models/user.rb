aca4122ed1d37d7247105e08aee3a5103baab136
glckfndr <obulan65@gmail.com>
2024-12-13 22:57:08 +0200

    feat: update User model to ensure all tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :tweets, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, length: { minimum: 3 }, uniqueness: true
