d9cdf7335b110ba23a57d2dec51653e7ce446dea
glckfndr <obulan65@gmail.com>
2024-12-17 12:28:28 +0200

    feat: add button to Home and styling

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import './Home.css';
 
 
 function Home() {
   return (
-    <div className="home-container">
-      <h1 className="home-title">Home Component</h1>
+    <div className="home">
+      <h1 className="home__title">Tweeter Site</h1>
+
+        <p className="home__description">
+          Environment for comunication of creative peoples
+        </p>
+            <hr className="home__divider"/>
+        <Link
+          to="/tweets"
+          className="home__link"
+          role="button"
+        >
+          View Tweets
+        </Link>
     </div>
   )
 }
