9aff465b45b522e093059594f1e4106385535332
glckfndr <obulan65@gmail.com>
2024-12-17 11:26:45 +0200

    fix: simplify Tweet validation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,14 +1,8 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  validates :content, presence: true, length: { maximum: 255 }
 
-  validate :validate_blank
+  validates :content, presence: true,
+      length: { maximum: 255 },
+      format: { without: /\A\s+\z/, message: "can't contain only blanks" }
 
-  private
-
-  def validate_blank
-    if content.blank? || content.match(/^\s+$/)
-      errors.add(:content, "can't be blank or contain only blanks")
-    end
-  end
 end
