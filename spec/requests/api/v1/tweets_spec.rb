274edc38e258930170a61ff0d0ba5d2b7c50278c
glckfndr <obulan65@gmail.com>
2024-12-18 18:12:21 +0200

    test: update Tweets request tests with Authentication

--- spec/requests/api/v1/tweets_spec.rb
+++ spec/requests/api/v1/tweets_spec.rb
@@ -29,4 +29,24 @@ RSpec.describe "Api::V1::Tweets", type: :request do
     end
   end
 
+  describe "Authentication" do
+    let(:user) { create(:user) }
+    let(:token) { Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first }
+    let(:headers) { { 'Authorization' => "Bearer #{token}" } }
+
+    describe 'GET /api/v1/tweets' do
+      it 'returns tweets for authenticated user' do
+        create_list(:tweet, 10, user: user)
+        get '/api/v1/tweets', headers: headers
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body).size).to eq(10)
+      end
+
+      it 'returns unauthorized without token' do
+        get '/api/v1/tweets'
+        expect(response).to have_http_status(:unauthorized)
+        expect(JSON.parse(response.body)).to include('errors')
+      end
+    end
+  end
 end
