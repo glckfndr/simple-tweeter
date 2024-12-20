3d2140b3f45595b8f72c155a2edde3a6a1b68eb6
glckfndr <obulan65@gmail.com>
2024-12-20 02:59:36 +0200

    config: update routes, devise, application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+      keys: [:username])
+  end
+
+  def authenticate_user!(options = {})
+    if user_signed_in?
+      super(options)
+    else
+      render json: { errors: ['You need to sign in or sign up before continuing.'] },
+        status: :unauthorized
+    end
+  end
 end
