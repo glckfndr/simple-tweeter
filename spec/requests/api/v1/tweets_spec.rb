41fbe35402479b709e889f820656a78f92f1786d
glckfndr <obulan65@gmail.com>
2024-12-20 03:04:45 +0200

    feat: add methods for tweet controller and fix tests.

--- spec/requests/api/v1/tweets_spec.rb
+++ spec/requests/api/v1/tweets_spec.rb
@@ -1,52 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Tweets", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/tweets/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/v1/tweets/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/api/v1/tweets/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/api/v1/tweets/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "Authentication" do
-    let(:user) { create(:user) }
+  let(:user) { create(:user) }
     let(:token) { Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first }
     let(:headers) { { 'Authorization' => "Bearer #{token}" } }
+  let(:tweet_params) { { tweet: { content: 'This is a test tweet' } } }
 
-    describe 'GET /api/v1/tweets' do
-      it 'returns tweets for authenticated user' do
-        create_list(:tweet, 10, user: user)
-        get '/api/v1/tweets', headers: headers
-        expect(response).to have_http_status(:ok)
-        expect(JSON.parse(response.body).size).to eq(10)
+  describe 'POST /api/v1/tweets' do
+    context 'with valid attributes' do
+      it 'creates a new tweet' do
+        expect {
+          post '/api/v1/tweets/create', params: tweet_params, headers: headers, as: :json
+        }.to change(Tweet, :count).by(1)
+        expect(response).to have_http_status(:created)
+        expect(JSON.parse(response.body)['content']).to eq('This is a test tweet')
       end
+    end
 
-      it 'returns unauthorized without token' do
-        get '/api/v1/tweets'
-        expect(response).to have_http_status(:unauthorized)
+    context 'with invalid attributes' do
+      it 'does not create a new tweet' do
+        invalid_params = { tweet: { content: '' } }
+        expect {
+          post '/api/v1/tweets/create', params: invalid_params, headers: headers, as: :json
+        }.not_to change(Tweet, :count)
+        expect(response).to have_http_status(:unprocessable_entity)
         expect(JSON.parse(response.body)).to include('errors')
       end
     end
   end
+
+  describe 'GET /api/v1/tweets/:id' do
+    let(:tweet) { create(:tweet, user: user) }
+
+    it 'returns the tweet' do
+      get "/api/v1/show/#{tweet.id}", headers: headers, as: :json
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)['content']).to eq(tweet.content)
+    end
+  end
+
+  describe 'DELETE /api/v1/tweets/:id' do
+    let!(:tweet) { create(:tweet, user: user) }
+
+    it 'deletes the tweet' do
+      expect {
+        delete "/api/v1/destroy/#{tweet.id}", headers: headers, as: :json
+      }.to change(Tweet, :count).by(-1)
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it 'returns an error if the tweet does not belong to the user' do
+      other_user = create(:user)
+      other_tweet = create(:tweet, user: other_user)
+      delete "/api/v1/destroy/#{other_tweet.id}", headers: headers, as: :json
+
+      #expect(response).to have_http_status(:unprocessable_entity)
+      expect(JSON.parse(response.body)).to include('errors')
+    end
+  end
+
+  describe "with authentication" do
+      it 'returns tweets with authorization token' do
+        create_list(:tweet, 10, user: user)
+        get '/api/v1/tweets/index', headers: headers
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body).size).to eq(10)
+      end
+
+      it 'returns unauthorized without authorization token' do
+        get '/api/v1/tweets/index'
+        expect(response).to have_http_status(:unauthorized)
+        expect(JSON.parse(response.body)).to include('errors')
+      end
+    end
 end
