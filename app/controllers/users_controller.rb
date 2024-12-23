676c08bda67dd7b3be88fbeeba6490e2afdbfb3a
glckfndr <obulan65@gmail.com>
2024-12-23 15:21:49 +0200

    refactor: user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,30 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :find_user
 
   def show
-    @user = User.find(params[:id])
     is_following = current_user.followees.include?(@user)
     render json: @user.as_json(include: { followers: { only: :username }, followees: { only: :username } }).merge(isFollowing: is_following, currentUser: current_user.username)
   end
 
   def follow
-    user = User.find(params[:id])
-    current_user.followees << user
-    render json: { notice: "Successfully followed #{user.username}.", currentUser: current_user.username }, status: :ok
+    current_user.followees << @user
+    render json: { notice: "Successfully followed #{@user.username}.", currentUser: current_user.username }, status: :ok
   end
 
   def unfollow
-    user = User.find(params[:id])
-    current_user.followees.delete(user)
-    render json: { notice: "Successfully unfollowed #{user.username}.", currentUser: current_user.username }, status: :ok
+    current_user.followees.delete(@user)
+    render json: { notice: "Successfully unfollowed #{@user.username}.", currentUser: current_user.username }, status: :ok
   end
 
   def followees
-    user = User.find(params[:id])
-    followees = user.followees
+
+    followees = @user.followees
     render json: { followees: followees.as_json(only: [:id, :username]) }
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
