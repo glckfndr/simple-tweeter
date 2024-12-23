ae78975501e7e3cc0f89ec2f089d3894681591b9
glckfndr <obulan65@gmail.com>
2024-12-23 22:09:00 +0200

    test: update tweet factory

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tweet do
-    content { "MyText" }
-    user { nil }
+    content { Faker::Lorem.sentence(word_count: 50).truncate(255) }
+    association :user
   end
 end
