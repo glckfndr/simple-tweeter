690cf0a210d28a7835c8eeec5e6a93106f7fd61e
glckfndr <obulan65@gmail.com>
2024-12-23 22:02:42 +0200

    feat: update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true, uniqueness: true
   has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
 
@@ -15,4 +14,34 @@ class User < ApplicationRecord
   has_many :followees, through: :followee_relationships, source: :followee
 
   has_many :retweets, dependent: :destroy
+
+  validates :username, presence: true, length: { minimum: 3 }, uniqueness: true
+  validates :email, uniqueness: true
+  validate :validate_email
+  validate :validate_password, if: -> { new_record? || !password.nil? }
+
+  private
+
+  def validate_email
+    email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    unless email =~ email_regex
+      errors.add(:email, "must be a valid email address")
+    end
+  end
+
+
+  def validate_password
+    return if password.blank?
+    if password.length < 8
+      errors.add(:password, "is too short (minimum is 8 characters)")
+    end
+
+    if password.scan(/\W/).empty?
+      errors.add(:password, "must contain at least one symbol")
+    end
+
+    if password =~ /(.)\1\1/
+      errors.add :password, 'cannot have any characters repeated more than twice'
+    end
+  end
 end
