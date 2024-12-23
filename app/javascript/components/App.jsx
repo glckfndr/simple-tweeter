91c021196a1f841a06f40b0fc5303f4455a1d641
glckfndr <obulan65@gmail.com>
2024-12-23 20:02:14 +0200

    fix: remove UseepPofile from App

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Tweets from './Tweets';
-import UserProfile from './UserProfile';
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Tweets />} />
-        <Route path="users/:id" element={<UserProfile />} />
       </Routes>
     </Router>
   )
